411fbb7f60af0228be16ed54056a72c4bb8cb257
Megan-Eagan <32804934+M-Eagan@users.noreply.github.com>
2018-07-28 10:59:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function countLongestName()
+    let name1= "Megan";
+    let name1length= name1.length;
+    let name2= "Joe";
+    let name2length= name2.length;
+
+    if (name1length > name2length) {
+    console.log(name1 + " has the longer name");
+    } else {
+    console.log(name1 + " has the longer name");
+    }
